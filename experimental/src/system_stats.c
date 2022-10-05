e60c23357a36373ff7d8005658e5200e3a41d4ba
Jason Shuhua Lei <lei56@ececomp3.ecn.purdue.edu>
2022-10-05 14:48:23 -0400

    Changed system stats printing formatting

--- experimental/src/system_stats.c
+++ experimental/src/system_stats.c
@@ -6,7 +6,8 @@ void print_system_stats(spine_t * spines, tor_t * tors) {
 }
 
 void print_spine_stats(spine_t * spines) {
-    printf("Spine stats: (spine_id : avg, max)\n");
+    int * max_spines = malloc(sizeof(int) * NUM_OF_SPINES);
+    double * avg_spines = malloc(sizeof(double) * NUM_OF_SPINES);
     for (int i = 0; i < NUM_OF_SPINES; i++) {
         spine_t spine = spines[i];
         spine_queue_stats_t spine_stat = spine->queue_stat;
@@ -24,14 +25,39 @@ void print_spine_stats(spine_t * spines) {
                 max_queue_len = j;
             }
         }
-        avg_queue_len = sum / count;
-        printf("%d : %0.3f, %d\n", i, avg_queue_len, max_queue_len);
+        avg_queue_len = (double) sum / count;
+        max_spines[i] = max_queue_len;
+        avg_spines[i] = avg_queue_len;
     }
+
+    printf("Max Queue Len at each Spine:\n[");
+    for(int i = 0; i < NUM_OF_SPINES; i++) {
+        if (i != 0) {
+            printf(", ");
+        }
+        printf("%d", max_spines[i]);
+    }
+    printf("]\n");
+
+    printf("Avg Queue Len at each Spine:\n[");
+    for(int i = 0; i < NUM_OF_SPINES; i++) {
+        if (i != 0) {
+            printf(", ");
+        }
+        printf("%0.4f", avg_spines[i]);
+    }
+    printf("]\n");
+
+    free(max_spines);
+    free(avg_spines);
     printf("\n");
 }
 
 void print_tor_stats(tor_t * tors) {
-    printf("ToR stats: (tor_id : up_avg, up_max, down_avg, down_max)\n");
+    int * up_tor_max = malloc(sizeof(int) * NUM_OF_TORS);
+    double * up_tor_avg = malloc(sizeof(double) * NUM_OF_TORS);
+    int * down_tor_max = malloc(sizeof(int) * NUM_OF_TORS);
+    double * down_tor_avg = malloc(sizeof(double) * NUM_OF_TORS);
     for (int i = 0; i < NUM_OF_TORS; i++) {
         tor_t tor = tors[i];
         tor_queue_stats_t tor_stat = tor->queue_stat;
@@ -50,7 +76,7 @@ void print_tor_stats(tor_t * tors) {
                 max_up_queue_len = j;
             }
         }
-        avg_up_queue_len = sum / count;
+        avg_up_queue_len = (double) sum / count;
 
         double avg_down_queue_len = 0;
         int max_down_queue_len = 0;
@@ -64,9 +90,53 @@ void print_tor_stats(tor_t * tors) {
                 max_down_queue_len = j;
             }
         }
-        avg_down_queue_len = sum / count;
+        avg_down_queue_len = (double) sum / count;
 
-        printf("%d : %0.3f, %d, %0.3f, %d\n", i, avg_up_queue_len, max_up_queue_len, avg_down_queue_len, max_down_queue_len);
+        up_tor_max[i] = max_up_queue_len;
+        up_tor_avg[i] = avg_up_queue_len;
+        down_tor_max[i] = max_down_queue_len;
+        down_tor_avg[i] = avg_down_queue_len;
     }
+
+    printf("Max Upstream Queue Len at each ToR:\n[");
+    for(int i = 0; i < NUM_OF_TORS; i++) {
+        if (i != 0) {
+            printf(", ");
+        }
+        printf("%d", up_tor_max[i]);
+    }
+    printf("]\n");
+
+    printf("Max Downstream Queue Len at each ToR:\n[");
+    for(int i = 0; i < NUM_OF_TORS; i++) {
+        if (i != 0) {
+            printf(", ");
+        }
+        printf("%d", down_tor_max[i]);
+    }
+    printf("]\n");
+
+    printf("Avg Upstream Queue Len at each ToR:\n[");
+    for(int i = 0; i < NUM_OF_TORS; i++) {
+        if (i != 0) {
+            printf(", ");
+        }
+        printf("%0.4f", up_tor_avg[i]);
+    }
+    printf("]\n");
+
+    printf("Avg Downstream Queue Len at each ToR:\n[");
+    for(int i = 0; i < NUM_OF_TORS; i++) {
+        if (i != 0) {
+            printf(", ");
+        }
+        printf("%0.4f", down_tor_avg[i]);
+    }
+    printf("]\n");
+
+    free(up_tor_max);
+    free(up_tor_avg);
+    free(down_tor_max);
+    free(down_tor_avg);
     printf("\n");
 }
